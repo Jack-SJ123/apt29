207a1424c3400a5cb68284e3f0daaab3cafe4551
Jack-SJ123 <ronaldo.sj0921@gmail.com>
2025-05-28 13:48:23 -0600

    Add APT29 scripts

---
+++ Set-WEC.ps1
@@ -0,0 +1,15 @@
+# Author: Roberto Rodriguez (@Cyb3rWard0g)
+# License: GPL-3.0
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$false)]
+    [string]$ServerAddresses
+)
+
+& .\Prepare-Box.ps1
+
+& .\Set-StaticIP.ps1 -ServerAddresses $ServerAddresses
+
+# Set Wallpaper
+& .\Set-WallPaper.ps1
